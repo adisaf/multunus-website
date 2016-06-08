1ac914a5682eaa6e8c0d8a1c3fb094516cb7a61f
Archana Raut <rautarchana9@gmail.com>
2016-06-08 17:00:12 +0530

    adds the line activate :directory_indexes line after blog block

--- config.rb
+++ config.rb
@@ -20,9 +20,6 @@ page '/*.txt', layout: false
 # Helpers
 ###
 
-# This will remove extentions like .html
-activate :directory_indexes
-
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   blog.prefix = "blog"
@@ -47,6 +44,8 @@ activate :blog do |blog|
   # blog.per_page = 10
   # blog.page_link = "page/{num}"
 end
+
+activate :directory_indexes
 
 page "/feed.xml", layout: false
 # Reload the browser automatically whenever files change
